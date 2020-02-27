7dbc8c3878c2a773f0301f6c2add26f475e9cd5a
James Barker <james.barker@cultureamp.com>
2020-02-27 20:49:12 +1100

    Use Wait Groups instead of timeouts for now

--- gorest.go
+++ gorest.go
@@ -2,11 +2,6 @@ package gorest
 
 import (
 	"sync"
-	"time"
-)
-
-const (
-	Timeout = 60 * time.Second
 )
 
 // RunTest main entry point
@@ -14,35 +9,34 @@ func RunTest(restTests []RestTest, workers int) (ResultTallys, []RestTestResult)
 	amountOfTests := len(restTests)
 
 	testCh := make(chan RestTest, 3000)
-	spiderCh := make(chan RestTest, 3000)
 	resultCh := make(chan RestTest, 3000)
 
-	var workerWG sync.WaitGroup
+	var workWG sync.WaitGroup // All the work to be done
+	var spiderWG sync.WaitGroup
+	var resultsWG sync.WaitGroup
+
 	for w := 1; w <= workers; w++ {
-		workerWG.Add(1)
-		go testWorker(&workerWG, testCh, spiderCh)
+		go testWorker(&workWG, testCh, resultCh)
 	}
 
-	var spiderWG sync.WaitGroup
-	spiderWG.Add(1)
-	go spiderWorker(&spiderWG, testCh, spiderCh, resultCh)
-
-	var resultsWG sync.WaitGroup
 	var allTests []RestTest
 	resultsWG.Add(1)
 	go resultWorker(&resultsWG, resultCh, &allTests)
 
 	// initial seed of tests to execute
 	for restTestIndex := 0; restTestIndex < amountOfTests; restTestIndex++ {
+		workWG.Add(1)
 		testCh <- restTests[restTestIndex]
 	}
 
-	// Wait for workers to finish - exit condition is no new tests in last 60 seconds, and no items in the spideCh
-	workerWG.Wait()
+	// Wait for work to finish
+	workWG.Wait()
+
+	// Clost chanels to tell workers they are done
+	close(testCh)
 
 	// Close all the channels to stop all the spiders and result workers
-	close(testCh)
-	close(spiderCh)
+
 	close(resultCh)
 
 	spiderWG.Wait()
@@ -58,46 +52,29 @@ func RunTest(restTests []RestTest, workers int) (ResultTallys, []RestTestResult)
 	return tally, results
 }
 
-func testWorker(wg *sync.WaitGroup, testCh chan RestTest, spiderCh chan RestTest) {
-	defer wg.Done()
+func testWorker(workWG *sync.WaitGroup, testCh chan RestTest, resultCh chan RestTest) {
+	for test := range testCh {
+		result := ExecuteAndVerify(test)
 
-	for true {
-		select {
-		case test := <-testCh:
-			result := ExecuteAndVerify(test)
-			spiderCh <- result
-
-		case <-time.After(Timeout):
-			if len(spiderCh) == 0 {
-				// we exit when we have had no new tests in the last Timeout
-				// AND there are nothing left in the spider (so not going to get any more)
-				return
-			}
-			// otherwise wait for some more tests
-		}
-	}
-}
-
-func spiderWorker(wg *sync.WaitGroup, testCh chan RestTest, spiderCh chan RestTest, resultCh chan RestTest) {
-	defer wg.Done()
-
-	for result := range spiderCh {
 		if len(result.RestTestResult.Errors) == 0 {
 			if result.Generator != nil {
 				newTests := result.Generator(result)
 
 				for _, newTest := range newTests {
+					workWG.Add(1)
 					testCh <- newTest
 				}
 			}
 		}
 
 		resultCh <- result
+
+		workWG.Done()
 	}
 }
 
-func resultWorker(wg *sync.WaitGroup, resultCh chan RestTest, allTests *[]RestTest) {
-	defer wg.Done()
+func resultWorker(resultsWG *sync.WaitGroup, resultCh chan RestTest, allTests *[]RestTest) {
+	defer resultsWG.Done()
 
 	// Get all the results
 	for test := range resultCh {
