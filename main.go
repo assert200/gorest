895f0b873869acdc9dd46d051bae5804f74146b0
Michael Ridgway <michael.ridgway@gmail.com>
2020-02-27 11:51:18 +1100

    Refactored to detect deadlock

--- main.go
+++ main.go
@@ -3,83 +3,107 @@ package gorest
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-var executeTestCh, testResultCh chan RestTest
+
+const (
+	SLEEP_FOR = 5 * time.Second
+	MAX_SLEEPS = 20
+)
+
 
 // RunTest main entry point
 func RunTest(restTests []RestTest, workers int) ResultTallys {
 	amountOfTests := len(restTests)
 
-	executeTestCh = make(chan RestTest, 100000)
-	testResultCh = make(chan RestTest, 100000)
-	defer close(executeTestCh)
-	defer close(testResultCh)
+	testCh := make(chan RestTest, 100)
+	spiderCh := make(chan RestTest, 100)
+	resultCh := make(chan RestTest, 100)
 
-	var testWorkerWG sync.WaitGroup
+	var workerWG sync.WaitGroup
 	for w := 1; w <= workers; w++ {
-		testWorkerWG.Add(1)
-		go testWorker(testWorkerWG, executeTestCh, testResultCh)
+		workerWG.Add(1)
+		go testWorker(workerWG, testCh, spiderCh)
 	}
 
-	var newTestWG sync.WaitGroup
-	for n := 1; n <= workers; n++ {
-		newTestWG.Add(1)
-		go newTestWorker(testWorkerWG, executeTestCh, testResultCh)
-	}
+	var spiderWG sync.WaitGroup
+	spiderWG.Add(1)
+	go spiderWorker(spiderWG, testCh, spiderCh, resultCh)
 
+	var resultsWG sync.WaitGroup
+	resultsWG.Add(1)
+	tally := &ResultTallys{}
+	go resultWorker(resultsWG, resultCh, tally)
+
+	// initial seed of tests to execute
 	for restTestIndex := 0; restTestIndex < amountOfTests;  restTestIndex++ {
-		// Only set tests off in batches of size of testWorker group
-		executeTestCh <- restTests[restTestIndex]
+		testCh <- restTests[restTestIndex]
 	}
 
 	// Wait for workers to finish
-	testWorkerWG.Wait()
-	newTestWG.Wait()
+	workerWG.Wait()
+	close(testCh)
+	close(spiderCh)
+	close(resultCh)
 
-	fmt.Println("Main waking up....")
+	spiderWG.Wait()
+	resultsWG.Wait()
 
-	// Get all the results
-	resultTallys := ResultTallys{}
-	for resultTally := range testResultCh {
-		resultTallys.Add(resultTally)
-	}
-
-	return resultTallys
+	// Return all the results
+	return *tally
 }
 
-func testWorker(wg sync.WaitGroup, executeTestCh chan RestTest, testResultCh chan<- RestTest) {
+func testWorker(wg sync.WaitGroup, testCh chan RestTest, spiderCh chan<- RestTest) {
 	defer wg.Done()
 
-	fmt.Println("Starting testWorker")
-	for test := range executeTestCh {
+	/*
+	for test := range testCh {
 		result := ExecuteAndVerify(test)
+		spiderCh <- result
+	}
+	 */
 
-		fmt.Println("testWorker: Adding result to channel")
-		testResultCh <- result
+	sleeps := 0
+	for sleeps < MAX_SLEEPS {
+		select {
+		case test := <- testCh:
+			sleeps = 0
+			result := ExecuteAndVerify(test)
+			spiderCh <- result
+		default:
+			fmt.Println("worker: no tests waiting...")
+			time.Sleep(SLEEP_FOR)
+			sleeps++
+		}
 	}
 
-	fmt.Println("Finished testWorker")
 }
 
-func newTestWorker(wg sync.WaitGroup, executeTestCh chan<- RestTest, testResultCh chan RestTest) {
+func spiderWorker(wg sync.WaitGroup, testCh chan<- RestTest, spiderCh chan RestTest, resultCh chan<- RestTest) {
 	defer wg.Done()
 
-	fmt.Println("Starting newTestWorker")
-	for result := range testResultCh {
+	for result := range spiderCh {
 		if len(result.RestTestResult.Errors) == 0 {
 			if result.Generator != nil {
 				newTests := result.Generator(result)
 
 				for _, newTest := range newTests {
-					fmt.Println("newTestWorker: Adding newTest to channel")
-					executeTestCh <- newTest
+					testCh <- newTest
 				}
 			}
 		}
-	}
 
-	fmt.Println("Finished newTestWorker")
+		resultCh <- result
+	}
 }
 
+func resultWorker(wg sync.WaitGroup, resultCh chan RestTest, resultTallys *ResultTallys) {
+	defer wg.Done()
 
+	// Get all the results
+	for resultTally := range resultCh {
+		resultTallys.Add(resultTally)
+	}
+
+}
